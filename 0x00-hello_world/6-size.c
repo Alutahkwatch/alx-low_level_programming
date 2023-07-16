89afdf3ed287174874da435807fe272e51fe28e4
Alutahkwatch <alutahjeff2017@gmail.com>
2023-07-16 10:39:50 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A program that prints the sithe size of various types on the computer it is compiled and run on
+ * main - A program thatuses sizeof funtion
  * return 0 (sucessful)
  */
 int main(void)
